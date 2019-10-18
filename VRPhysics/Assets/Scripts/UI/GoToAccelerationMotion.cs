cf08659e5ba548fd8050759fc6267fc6a77e8a5f
Inspiron5980 <puyu986@fuwamofu.com>
2019-10-18 14:52:18 +0900

    [add][update]シーン遷移実装

---
+++ VRPhysics/Assets/Scripts/UI/GoToAccelerationMotion.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToAccelerationMotion : MonoBehaviour, VRUI
+{
+    public void Receiver()
+    {
+        SceneManager.LoadScene("AccelerationMotion");
+    }
+}
