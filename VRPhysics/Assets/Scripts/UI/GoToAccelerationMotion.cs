2c25ebdc2ecdedcc2447f0e9ea77a726f4d6990a
Inspiron5980 <puyu986@fuwamofu.com>
2019-10-21 15:01:28 +0900

    [update]Rayが当たった判定関数の実装関数

--- VRPhysics/Assets/Scripts/UI/GoToAccelerationMotion.cs
+++ VRPhysics/Assets/Scripts/UI/GoToAccelerationMotion.cs
@@ -3,6 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class GoToAccelerationMotion : MonoBehaviour, VRUI
 {
+
+    [SerializeField] Animator animator;
+    bool isRayStaying;
+
+    public void Reaction()
+    {
+        if (isRayStaying)
+        {
+            return;
+        }
+
+
+    }
+
     public void Receiver()
     {
         SceneManager.LoadScene("AccelerationMotion");
