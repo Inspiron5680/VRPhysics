3c96d1e0d0e61d401e751e9daf1ccf948f013f47
Inspiron5980 <puyu986@fuwamofu.com>
2019-10-21 15:42:16 +0900

    [update]コミット忘れ

--- VRPhysics/Assets/Scripts/UI/GoToAccelerationMotion.cs
+++ VRPhysics/Assets/Scripts/UI/GoToAccelerationMotion.cs
@@ -5,17 +5,10 @@ public class GoToAccelerationMotion : MonoBehaviour, VRUI
 {
 
     [SerializeField] GameObject accelerationMotionDemo;
-    bool isRayStaying;
 
     public void Reaction()
     {
-        if (isRayStaying)
-        {
-            return;
-        }
-
         accelerationMotionDemo.SetActive(true);
-        isRayStaying = true;
     }
 
     public void Receiver()
