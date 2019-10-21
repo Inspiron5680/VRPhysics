a10c868c166aeba731288c75caee7fa231148ca9
Inspiron5980 <puyu986@fuwamofu.com>
2019-10-21 16:12:46 +0900

    [add]ホームへ戻るUIオブジェクトを追加

---
+++ VRPhysics/Assets/Scripts/UI/ReturnSceneObserve.cs
@@ -0,0 +1,26 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class ReturnSceneObserve : MonoBehaviour
+{
+    [SerializeField] GameObject returnSceneUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.UpdateAsObservable()
+            .Where(_ => OVRInput.GetDown(OVRInput.RawButton.Start))
+            .Subscribe(_ =>
+            {
+                if (returnSceneUI.activeInHierarchy)
+                {
+                    returnSceneUI.SetActive(false);
+                }
+                else
+                {
+                    returnSceneUI.SetActive(true);
+                }
+            });
+    }
+}
