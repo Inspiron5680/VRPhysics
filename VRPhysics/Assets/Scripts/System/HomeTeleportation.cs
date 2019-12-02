07ae0fcad147219f46d86d4a12e908baec12fbd5
Inspiron5980 <puyu986@fuwamofu.com>
2019-12-02 13:21:59 +0900

    [add]ステージモデルとテレポートモデル、それぞれの動作スクリプトの追加

---
+++ VRPhysics/Assets/Scripts/System/HomeTeleportation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeTeleportation : MonoBehaviour
+{
+    string nextSceneName;
+
+    public void SetNextScene(string sceneName)
+    {
+        nextSceneName = sceneName;
+    }
+
+    public void GotoNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
---
+++ VRPhysics/Assets/Scripts/UI/GoToSelectScene.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GoToSelectScene : MonoBehaviour,VRUI
+{
+    [SerializeField] string[] sceneNames;
+    [SerializeField] GameObject teleportation;
+    [SerializeField] Transform targetObject;
+    public static int SceneNamesIndex;
+
+    public void Receiver()
+    {
+        if (sceneNames.Length == 0)
+        {
+            return;
+        }
+
+        if (SceneNamesIndex >= sceneNames.Length)
+        {
+            SceneNamesIndex = 0;
+        }
+
+        if (SceneNamesIndex < 0)
+        {
+            SceneNamesIndex = sceneNames.Length - 1;
+        }
+
+        if (sceneNames[SceneNamesIndex] == "Null")
+        {
+            return;
+        }
+
+        var instanceTeleportation = Instantiate(teleportation, new Vector3(targetObject.position.x, 0, targetObject.position.z), Quaternion.identity);
+        instanceTeleportation.GetComponent<HomeTeleportation>().SetNextScene(sceneNames[SceneNamesIndex]);
+        Destroy(transform.parent.gameObject);
+    }
+
+    public void Reaction()
+    {
+        return;
+    }
+}
